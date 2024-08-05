b7052d549b8b2041a26461075c3f6da42da4cefc
Erick Clinton <erickleite2009321@gmail.com>
2024-08-05 09:44:46 -0300

    feat: create method for find cpfcnpj or email

--- src/main/java/tech/erick/picpay/module/wallet/repositories/WalletRepository.java
+++ src/main/java/tech/erick/picpay/module/wallet/repositories/WalletRepository.java
@@ -2,6 +2,8 @@ package tech.erick.picpay.module.wallet.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.erick.picpay.module.wallet.entity.WalletEntity;
+import java.util.Optional;
 
 public interface WalletRepository extends JpaRepository<WalletEntity,Long> {
+    Optional<WalletEntity> findByCpfCnpjOrEmail(String cpfOrCnpj, String email);
 }
