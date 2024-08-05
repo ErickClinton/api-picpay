82ce88910a5b3acc988d47a1cb9ee36a44df9a45
Erick Clinton <erickleite2009321@gmail.com>
2024-08-05 09:44:23 -0300

    feat: controller for create user

---
+++ src/main/java/tech/erick/picpay/module/wallet/WalletController.java
@@ -0,0 +1,24 @@
+package tech.erick.picpay.module.wallet;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import tech.erick.picpay.module.wallet.dto.CreateWalletDto;
+import tech.erick.picpay.module.wallet.entity.WalletEntity;
+
+@RestController
+public class WalletController {
+
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @PostMapping("/wallets")
+    public ResponseEntity<WalletEntity> create(@RequestBody CreateWalletDto createWalletDto){
+        var wallet = walletService.create(createWalletDto);
+        return ResponseEntity.ok(wallet);
+    }
+}
